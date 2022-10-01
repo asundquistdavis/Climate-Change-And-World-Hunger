b041a8a7ed6f19fff5207be18900d649b85501d3
Andrew <asundquistdavis@gmail.com>
2022-10-01 13:18:25 -0500

    readme/proposal

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+from data import get_foods as gf
+from data import get_temps as gt
+
+app = Flask(__name__)
+
+# index route which serves dashboard for websites
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# data route which serves data from database
+@app.route('/api/v1.0/temps')
+def temps():
+
+    # call get_temps method
+    temps = gt()
+
+    # return data
+    return temps
+
+@app.route('/api/v1.0/foods')
+def foods():
+
+    # call get_foods method
+    foods = gf()
+
+    # return data
+    return foods
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
